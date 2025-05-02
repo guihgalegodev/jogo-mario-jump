43d43157900ce1f8255f0b3f081c287c5bc11b98
guihgalegodev <guigalego96@gmail.com>
2025-05-02 20:22:58 -0300

    hitbox da tela acima de 1200px desktop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 function marioPular(e) {
   if (e.type === "keydown") {
@@ -16,6 +17,25 @@ function marioPular(e) {
   }
 }
 
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 200) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./imagens/game-over.png";
+    mario.style.width = "75px";
+    mario.style.left = "50px";
+
+    clearInterval(loop);
+  }
+}, 10);
+
 window.addEventListener("keydown", marioPular);
 window.addEventListener("touchstart", marioPular);
-console.log(window.innerHeight);
