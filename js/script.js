6056b4d63ba5fb7c9e4cc677c65965fb604e16fa
guihgalegodev <guigalego96@gmail.com>
2025-05-01 22:51:25 -0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const mario = document.querySelector(".mario");
+
+function marioPular(e) {
+  if (e.type === "keydown") {
+    if (e.key === " ") {
+      mario.classList.add("pular");
+      setTimeout(() => {
+        mario.classList.remove("pular");
+      }, "500");
+    }
+  } else if (e.type === "touchstart") {
+    mario.classList.add("pular");
+    setTimeout(() => {
+      mario.classList.remove("pular");
+    }, "500");
+  }
+}
+
+window.addEventListener("keydown", marioPular);
+window.addEventListener("touchstart", marioPular);
